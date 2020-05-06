172835271196cf0f38485855761be72e2fdd8b22
kavi-appveen <kavi@appveen.com>
2020-05-07 02:06:00 +0530

    bundling building mocking

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,13 +1,20 @@
 import express from 'express';
 import path from 'path';
-import open from 'open';
+//import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
+import bodyParser from 'body-parser';
 
 /* eslint-disable no-console */
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 const app = express();
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
 
 const compiler = webpack(config);
 
@@ -16,14 +23,35 @@ app.use(require('webpack-dev-middleware')(compiler, {
   publicPath: config.output.publicPath
 }))
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.listen(port, function(err) {
-  if(err) {
+app.get('/users', function (req, res) {
+  res.json([
+    { "id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com" },
+    { "id": 2, "firstName": "Tammy", "lastName": "Norton", "email": "tnorton@yahoo.com" },
+    { "id": 3, "firstName": "Tina", "lastName": "Lee", "email": "lee.tina@hotmail.com" }
+  ]);
+})
+
+app.post('/data', function (req, res) {
+  var jsonData = req.body || req.data;
+  console.log("JSon Data - ", jsonData)
+  var returnData = {};
+
+  Object.keys(jsonData).map(key => {
+    returnData[`Employee${key}`] = jsonData[key];
+  });
+
+  res.send(returnData);
+})
+
+app.listen(port, function (err) {
+  if (err) {
     console.log(err);
   } else {
-    open('http://localhost:' + port);
+    // open('http://localhost:' + port);
+    console.log('server running at http://localhost:' + port);
   }
 });
